0d69d3d5c474cfaac56a4298dcd1c9536fa7b0ee
yupliang <yupeiliang110@163.com>
2014-08-25 16:08:03 +0800

    initial Product

---
+++ models/Product.m
@@ -0,0 +1,13 @@
+//
+//  Product.m
+//  SimulatedEcosystem
+//
+//  Created by YPL on 14-8-25.
+//  Copyright (c) 2014年 app. All rights reserved.
+//
+
+#import "Product.h"
+
+@implementation Product
+
+@end
